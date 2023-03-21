24c2dc4c02acdb542a4eb1c350ee507c03a47682
rean <rean@vip.qq.com>
2023-03-21 09:51:31 +0800

    term窗口快捷键重定义

--- plugins/core.lua
+++ plugins/core.lua
@@ -21,6 +21,7 @@ return {
     end,
   },
   {"reanxp/neovim-ayu"},
+  {"akinsho/toggleterm.nvim", opts = {open_mapping = [[<C-\>]]} },
   -- You can disable default plugins as follows:
   -- { "max397574/better-escape.nvim", enabled = false },
   --
